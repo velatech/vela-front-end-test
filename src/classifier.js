a660a53edb4ea5dcaee4444d6a364ef3be817f42
Jerry Hanks <jerryhanksokafor@gmail.com>
2019-07-26 12:32:21 +0100

    Add untracked files

---
+++ src/classifier.js
@@ -0,0 +1,11 @@
+/**
+ * This is the entry point to the program
+ *
+ * Question 1
+ * @param {any} input Array of student objects
+ */
+function classifier(input) {
+  // Your code goes here
+}
+
+module.exports = classifier;
---
+++ src/laundry.js
@@ -0,0 +1,14 @@
+/**
+ * This is the entry point to your program
+ *
+ * Question 2
+ *
+ * @param {number} noOfWashes The number of washes the machine can wash
+ * @param {number[]} cleanPile Array containing the number of clean socks
+ * @param {number[]} dirtyPile Array containing the number of dirty socks
+ */
+function getMaxPairs(noOfWashes, cleanPile, dirtyPile) {
+  // Your code goes here
+}
+
+module.exports = getMaxPairs;
